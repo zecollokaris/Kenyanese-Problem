04ffd4434cf905eddb6d0aadc782a34254a4c750
zecollokaris <collo.kariss@gmail.com>
2019-02-14 14:10:27 +0300

    added input to BDD

---
+++ kenyanese.java
@@ -0,0 +1,15 @@
+// BDD
+
+// There are three mappings
+// a -> y
+// o -> e
+// z -> q
+
+// For the inputs below they contain letters from a -> z
+
+// Input
+
+// ejp mysljylc kd kxveddknmc re jsicpdrysi
+// rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd
+// de kr kd eoya kw aej tysr re ujdr lkgc jv
+

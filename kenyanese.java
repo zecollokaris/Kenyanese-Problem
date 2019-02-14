3b6d254dec00445281e97d55559bef1c28f430dd
zecollokaris <collo.kariss@gmail.com>
2019-02-14 14:26:48 +0300

    added letter switches

--- kenyanese.java
+++ kenyanese.java
@@ -1,4 +1,4 @@
-// BDD
+// BDD!
 
 // There are three mappings
 // a -> y
@@ -7,7 +7,7 @@
 
 // For the inputs below they contain letters from a -> z
 
-// Input
+// Input!
 
 // Case #1: ejp mysljylc kd kxveddknmc re jsicpdrysi
 // Case #2: rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd
@@ -15,8 +15,39 @@
 
 // This input was used to get the relevant output translation for Kenyanese!
 
-// Output
+// Output!
 
 // Case #1: our language is impossible to understand
 // Case #2: there are twenty six factorial possibilities
 // Case #3: so it is okay if you want to just give up
+
+// USING THE IN PUT ABOVE WE MATCH IT TO THE OUTPUT TO GET THE LETTERS SWITCH!
+
+// BELOW ARE HOW THE LETTERS SWITCH!
+
+//        a=y
+//        b=h
+//        c=e
+//        d=s
+//        e=o
+//        f=c
+//        g=v
+//        h=x
+//        i=d
+//        j=u
+//        k=i
+//        l=g
+//        m=l
+//        n=b
+//        o=k
+//        p=r
+//        q=z
+//        r=t
+//        s=n
+//        t=w
+//        u=j
+//        v=p
+//        w=f
+//        x=m
+//        y=a
+//        z=q

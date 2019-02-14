ef3a0c8aafabb3eac90ebcef8eb8e960bce73b5c
zecollokaris <collo.kariss@gmail.com>
2019-02-14 14:12:35 +0300

    added output

--- kenyanese.java
+++ kenyanese.java
@@ -9,7 +9,14 @@
 
 // Input
 
-// ejp mysljylc kd kxveddknmc re jsicpdrysi
-// rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd
-// de kr kd eoya kw aej tysr re ujdr lkgc jv
+// Case #1: ejp mysljylc kd kxveddknmc re jsicpdrysi
+// Case #2: rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd
+// Case #3: de kr kd eoya kw aej tysr re ujdr lkgc jv
 
+// This input was used to get the relevant output translation for Kenyanese!
+
+// Output
+
+// Case #1: our language is impossible to understand
+// Case #2: there are twenty six factorial possibilities
+// Case #3: so it is okay if you want to just give up

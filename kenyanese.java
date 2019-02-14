70aaa37501e7add7c956e5dafd35a7a211c3da3d
zecollokaris <collo.kariss@gmail.com>
2019-02-14 16:50:55 +0300

    added code .

--- kenyanese.java
+++ kenyanese.java
@@ -56,3 +56,124 @@
 
 // CODE SOLUTION!
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Kenyanese {
+
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        int lines = 0;
+
+        Scanner in = new Scanner(new File("A small attempt.in"));
+        lines = in.nextInt();
+        in.nextLine();
+        for (int line = 0; line < lines; ++line) {
+            String curLine = in.nextLine();
+            String output = translate(curLine);
+            System.out.println("Case #" + (line + 1) + ": " + output);
+        }
+    }
+
+    private static String translate(String curLine) {
+        String output = "";
+        for (int i = 0; i < curLine.length(); ++i) {
+            output += "" + charMapping(curLine, i);
+        }
+        return output;
+    }
+
+    private static char charMapping(String curLine, int i) {
+        char c = curLine.charAt(i);
+        switch (c) {
+        case 'a':
+            c = 'y';
+            break;
+        case 'b':
+            c = 'h';
+            break;
+        case 'c':
+            c = 'e';
+            break;
+        case 'd':
+            c = 's';
+            break;
+        case 'e':
+            c = 'o';
+            break;
+        case 'f':
+            c = 'c';
+            break;
+        case 'g':
+            c = 'v';
+            break;
+        case 'h':
+            c = 'x';
+            break;
+        case 'i':
+            c = 'd';
+            break;
+        case 'j':
+            c = 'u';
+            break;
+        case 'k':
+            c = 'i';
+            break;
+        case 'l':
+            c = 'g';
+            break;
+        case 'm':
+            c = 'l';
+            break;
+        case 'n':
+            c = 'b';
+            break;
+        case 'o':
+            c = 'k';
+            break;
+        case 'p':
+            c = 'r';
+            break;
+        case 'q':
+            c = 'z';
+            break;
+        case 'r':
+            c = 't';
+            break;
+        case 's':
+            c = 'n';
+            break;
+        case 't':
+            c = 'w';
+            break;
+        case 'u':
+            c = 'j';
+            break;
+        case 'v':
+            c = 'p';
+            break;
+        case 'w':
+            c = 'f';
+            break;
+        case 'x':
+            c = 'm';
+            break;
+        case 'y':
+            c = 'a';
+            break;
+        case 'z':
+            c = 'q';
+            break;
+        default:
+            
+            break;
+        }
+        return c;
+    }
+
+}
